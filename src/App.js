2f724a46f495f450ca67a328495d1637d1ef7c6a
Ansar-Thangalparambil <ansartpp@gmail.com>
2024-08-15 23:10:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+  // js code need to enter here
+  return (
+    // jsx code need to enter here
+<> {/* Empty tag  jsx expression Every element inside return must be in a single parent element */}
+ {/*  jsx expression Every element inside return must be in a single parent element */}
+      <h1>Hi hello</h1>
+      <div className="App">
+       <h1>CampusCracker</h1>
+       <Example/>
+      </div>
+</>
+  );
+}
+
+export default App;
