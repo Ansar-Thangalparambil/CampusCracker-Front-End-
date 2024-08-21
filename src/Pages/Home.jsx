644a6b344ad7701677e57f26e192ab42d0db9e3d
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-08-21 15:30:18 +0530

    Home,login,sugnup pages designs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import './Home.css'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className="home container my-5 " >
+
+        <div className='row mt-5'>
+          <div className="col-md-12 d-flex justify-content-center">
+            
+            <div class="mb-3 w-100 rounded-5 overflow-hidden  shadow" style={{backgroundColor:"white"}}>
+              <div class="row g-0">
+                <div class="col-md-5 ">
+                  <img src="https://images.shiksha.com/mediadata/images/articles/1565072398phpMhDAk7.jpeg"  alt="... " className=' img-fluid'/>
+                </div>
+                <div class="col-md-7 text-start d-flex justify-content-center flex-column">
+                  <div class="p-3 text-dark">
+                    <h5 class="">General Aptitude</h5>
+                    <p class="">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    {/* <p class="card-text"><small class="">Last updated 3 mins ago</small></p> */}
+                    <button className='btn-info border-0 rounded-5 px-3 py-2 fw-semibold '>Show more...</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className='row mt-5'>
+          <div className="col-md-12 d-flex justify-content-center">
+            
+            <div class="mb-3 w-100 rounded-5 overflow-hidden  shadow" style={{backgroundColor:"white"}}>
+              <div class="row g-0">
+                <div class="col-md-5 ">
+                  <img src="https://www.jobtestsuccess.com/wp-content/uploads/2019/11/Inductive-Reasoning-Test-1.jpg"  alt="... " className=' img-fluid'/>
+                </div>
+                <div class="col-md-7 text-start d-flex justify-content-center flex-column">
+                  <div class="p-3 text-dark">
+                    <h5 class="">Verbal Reasoning</h5>
+                    <p class="">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    {/* <p class="card-text"><small class="">Last updated 3 mins ago</small></p> */}
+                    <button className='btn-success border-0 rounded-5 px-3 py-2 fw-semibold '>Show more...</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className='row mt-5'>
+          <div className="col-md-12 d-flex justify-content-center">
+            
+            <div class="mb-3 w-100 rounded-5 overflow-hidden  shadow" style={{backgroundColor:"white"}}>
+              <div class="row g-0">
+                <div class="col-md-5 ">
+                  <img src="https://elearningindustry.com/wp-content/uploads/2017/12/5-advantages-of-online-mock-tests-1.png"  alt="... " className=' img-fluid'/>
+                </div>
+                <div class="col-md-7 text-start d-flex justify-content-center flex-column">
+                  <div class="p-3 text-dark">
+                    <h5 class="">Onlne Tests</h5>
+                    <p class="">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    {/* <p class="card-text"><small class="">Last updated 3 mins ago</small></p> */}
+                    <button className='btn-dark text-light border-0 rounded-5 px-3 py-2 fw-semibold '>Show more...</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <Footer/>
+    </>
   )
 }
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,80 +2,46 @@ import React from 'react'
 import Title from '../components/Title'
 import './Loginpage.css'
 import { Link } from 'react-router-dom'
+import Register from './Register'
 function Login({register}) { /* data passed for register */
+
   const regForm = register?true:false /* Assigned a constant variable True if reg present false if not present */
+
   return (
     <div className='total d-flex justify-content-center flex-column align-items-center'>
-      <div className='box'>
-        <div className="headttl">
+      <div className='box px-5 py-4 col-md-4'>
+        <div className="headttl mb-3">
           <Title/>
         </div >
         {regForm? /* Checking if register exist or not if yes render code below ie register page */
-          <div className="input-btn"> 
+          <div className="input-btn "> 
 
-         <div className="fullname">
-          <input type="text" placeholder='Fullname' />
-         </div>
-
-         <div className="email">
-          <input type="email" placeholder='Enter your E-mail Address' />
-         </div>
-
-         <div className="regno">
-          <input type="number" placeholder='Enter your College Registration Number' />
-         </div>
-
-         <div className="dept">
-          <input type="text" placeholder='Select your Department' />
-         </div>
-
-         <div className="year">
-          <input type="number" placeholder='Select your Year of Study' />
-         </div>
-
-          <div className="ip1">
-            <input type="text" placeholder='Username'/>
-          </div>
-          
-          <div className="ip2">
-            <input type="password" placeholder='Password'/>
-          </div> 
-          
-          <div className="logbtn d-flex justify-content-center ">
-          <button className=' d-flex gap-3 align-items-center px-4  border-0 fw-semibold rounded-5'>
-                REGISTER
-          </button>
-          </div>
-
-          <div className="yesorno">
-            <p>Already have an account? <Link to='/login'>Login</Link></p>
-          </div>
+         <Register/>
 
         </div>
         : /* If register not exist render code below ie login page */
-        <div className="input-btn">
+        <div className="input-btn gap-3 d-flex flex-column">
 
-          <div className="ip1">
+          <div className="ip-fields">
             <input type="text" placeholder='Username'/>
           </div>
 
-          <div className="ip2">
+          <div className="ip-fields">
             <input type="password" placeholder='Password'/>
           </div> 
           
-          <div className="logbtn d-flex justify-content-center ">
-            <button className=' d-flex gap-3 align-items-center px-4  border-0 fw-semibold rounded-5'>
+          <div className="logbtn">
+            <button className='rounded-5 d-flex justify-content-center fw-semibold py-2 w-100'>
                   LOGIN
             </button>
           </div>
 
-          <div className="yesorno">
+          <div className="yesorno d-flex justify-content-center">
             <p>Don't have an account? <Link to='/register'>SignUp</Link></p>
           </div>
 
         </div>
         }
-        <div className="noaccount"></div>
       </div>
   
     </div>
