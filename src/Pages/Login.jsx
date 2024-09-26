de0f2e8819546c3dcac29835ab65a99f7b634622
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-09-27 01:38:15 +0530

    Login finished

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import './Loginpage.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Register from './Register'
+import { loginAPI } from '../services/allAPI'
 function Login({register}) { /* data passed for register */
 
   const regForm = register?true:false /* Assigned a constant variable True if reg present false if not present */
+
+  const [userData,setUserData] = useState({
+    fullname:"",
+    email:"",
+    regnno:"",
+    department:"",
+    yearofstudy:"",
+    username:"",
+    password:""
+  })
+
+  const navigate = useNavigate()
+
+  //function to login
+
+  const handlelogin = async(e)=>{
+    e.preventDefault()
+    const {email,password} =userData
+    if(!email || !password){
+      alert("Fill the form Completely")
+    }
+    else{
+      const result = await loginAPI(userData)
+      console.log(result);
+
+      if (result.status===200) {
+        //store data
+        // in session storage; key:string, value:string
+        sessionStorage.setItem("existingUser",JSON.stringify(result.data.existingUser))
+        sessionStorage.setItem("token",result.data.token)
+
+        alert('Login Successful')
+
+        setUserData({
+          email:"",password:""
+        })
+        navigate('/home')
+      }
+      else{
+        alert(result.response.data)
+      }
+
+    }
+  }
 
   return (
     <div className='total d-flex justify-content-center flex-column align-items-center'>
@@ -23,15 +68,15 @@ function Login({register}) { /* data passed for register */
         <div className="input-btn gap-3 d-flex flex-column">
 
           <div className="ip-fields">
-            <input type="text" placeholder='Username'/>
+            <input type="text" placeholder='Enter Your Email' value={userData.email} onChange={(e)=>setUserData({...userData,email:e.target.value})}/>
           </div>
 
           <div className="ip-fields">
-            <input type="password" placeholder='Password'/>
+            <input type="password" placeholder='Password' value={userData.password} onChange={(e)=>setUserData({...userData,password:e.target.value})}/>
           </div> 
           
           <div className="logbtn">
-            <button className='rounded-5 d-flex justify-content-center fw-semibold py-2 w-100'>
+            <button onClick ={handlelogin} className='rounded-5 d-flex justify-content-center fw-semibold py-2 w-100'>
                   LOGIN
             </button>
           </div>
