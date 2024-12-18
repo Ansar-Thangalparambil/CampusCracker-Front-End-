9f0a1113e13fad19336d726684aaeddb98015be5
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-12-18 14:07:14 +0400

    Landing page design modified

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -2,28 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import learning from '../assets/learning-hd.png'
 import './Css/Landingpage.css'
-import Header from '../components/Header'
+// import Header from '../components/Header'
 import Footer from '../components/Footer'
+import ccLogoBlack from '../assets/ccLogoBlack.png'
 
 function Landingpage() {
   return (
     <div className=''> 
-    <Header/>
-      <main className=" gap-5 d-flex align-items-center mb-3 px-5" >
-        <div className="img">
-          <img src={learning} alt="" height={'455px'} />
+    {/* <Header/> */}
+    <div className="title-name text-center pt-2 pb-3">
+      <img src={ccLogoBlack} alt="" />
+    </div>
+    
+      <main className="  d-flex flex-column align-items-center mb-3 px-5" >
+
+        <div className="img"> 
+          <img src={learning} alt="" height={'300px'} />
         </div>
-        <div className="desc-btn ">
-          <p style={{fontSize:"25px",textAlign:'justify',width:'100%'}} className=''>CampusCracker is an innovative and comprehensive platform designed to revolutionize the way students prepare for campus placements and competitive exams. Tailored specifically for students of MES College of Engineering, Kuttipuram.</p>
+
+        <div className="desc-btn gap-2 d-flex flex-column mt-3 text-center align-items-center ">
+
+          <span className=''><b>CampusCracker</b> is an innovative and comprehensive platform designed to revolutionize <br /> the way students prepare for campus placements and competitive exams. Tailored specifically for students of <br /> MES College of Engineering, Kuttipuram.</span>
          
-            <Link to={'/login'} className='linkline d-flex justify-content-start d-inline-flex'>
-              <button className=' d-flex gap-3 align-items-center px-4 py-2 border-0 fw-semibold rounded-5 text-center'>
+          <div className='d-inline-flex justify-content-center'>
+            <Link to={'/login'} className='linkline d-flex justify-content-center '>
+              <button className=' d-flex gap-3 align-items-center px-3 py-1 border-0  rounded-5 text-center'>
                 Let's Crack It! 
                 <i class="fa-solid fa-paper-plane"></i>
               </button>
             </Link>
+          </div>
          
         </div>
+
       </main>
       <Footer/>
     </div> 
